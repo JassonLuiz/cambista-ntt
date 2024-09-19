0a34bf915a25df07a35944ad4df5cf96ba0d488f
Jasson Luiz <jassonluizjr@hotmail.com>
2024-09-19 18:34:06 -0300

    Primeiro commit

---
+++ src/main/java/br/cambista/CambistaApplication.java
@@ -0,0 +1,13 @@
+package br.cambista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/cambista/CambistaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cambista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
